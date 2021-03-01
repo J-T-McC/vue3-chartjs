b13af0bec6090981570f21434ab487e9f1699c32
J-T-McC <tyson_mcc@hotmail.com>
2021-02-28 17:38:10 -0800

    Reverted package.json dependency change
    Added chart.js as external library in vite rollup

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['vue'],
+      external: ['vue', 'chart.js'],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
